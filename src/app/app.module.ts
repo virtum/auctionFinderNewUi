fd272044f7b9176299ef36029473a2e67e3e916c
Virtum <pawel.filocha@gmail.com>
2018-08-09 07:14:59 +0200

    Change imports in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import { AppComponent } from './app.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
+
 import { HomeComponent } from './home/home.component';
 import { AccountComponent } from './account/account.component';
 
@@ -20,12 +31,8 @@ import { AccountComponent } from './account/account.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
     LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: 'account', component: AccountComponent },
